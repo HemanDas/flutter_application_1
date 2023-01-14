b5e64c2fbcd6fa5aa671a926bc08c4edc10ec224
uttamraja <76909757+uttamraja@users.noreply.github.com>
2023-01-14 12:39:40 +0545

    changed title

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class _FitnexState extends State<Fitnex> {
     return Scaffold(
       drawer: Sidebar(),
       appBar: AppBar(
-        title: const Text('This is Fitnex projectG'),
+        title: const Text('Fitnex'),
         toolbarHeight: 40.0,
         centerTitle: true,
         backgroundColor: Color(0xff5ac18e),

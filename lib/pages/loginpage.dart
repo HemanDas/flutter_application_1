00a62f8d88c75eaaa9e1c26cc4b4b07493ddc5a8
hemandas630@gmail.com <hemandas630@gmail.com>
2023-01-28 17:52:40 +0545

    removed the register code from it

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 // import 'package:flutter_application_1/pages/Sidebar.dart';
-import 'package:flutter_application_1/main.dart';
+import 'package:flutter_application_1/homepage.dart';
 import 'package:flutter_application_1/pages/container.dart';
-
-class loginpage extends StatefulWidget {
-  const loginpage({super.key});
-
-  @override
-  State<loginpage> createState() => _loginpageState();
-}
+import 'package:flutter_application_1/pages/registerpage.dart';
 
 Widget buildemail() {
   return Column(
@@ -187,6 +181,13 @@ Widget buildpasswordreenter() {
   );
 }
 
+class loginpage extends StatefulWidget {
+  const loginpage({super.key});
+
+  @override
+  State<loginpage> createState() => _loginpageState();
+}
+
 class _loginpageState extends State<loginpage> {
   bool? isRememberMe = false;
 
@@ -255,7 +256,7 @@ class _loginpageState extends State<loginpage> {
               },
             ),
           ),
-          Text(
+          const Text(
             'Remember Me',
             style: TextStyle(
               color: Colors.white,
@@ -269,12 +270,12 @@ class _loginpageState extends State<loginpage> {
 
   Widget buildloginbutton() {
     return Container(
-      padding: EdgeInsets.symmetric(vertical: 25),
+      padding: const EdgeInsets.symmetric(vertical: 25),
       width: double.infinity,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           primary: Colors.white,
-          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+          padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
         ),
         onPressed: () {
           Navigator.push(
@@ -365,113 +366,6 @@ class _loginpageState extends State<loginpage> {
   }
 }
 
-class signup extends StatefulWidget {
-  const signup({super.key});
-
-  @override
-  State<signup> createState() => _signupState();
-}
-
-class _signupState extends State<signup> {
-  Widget buildsignupbutton() {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 25),
-      width: double.infinity,
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          primary: Colors.white,
-          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-        ),
-        onPressed: () {
-          // Navigator.push(
-          //   context,
-          //   MaterialPageRoute(
-          //     builder: (context) => Fitnex(),
-          //   ),
-          // );
-        },
-        child: const Text(
-          'Register',
-          style: TextStyle(
-            color: Color(0xff5ac18e),
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: AnnotatedRegion<SystemUiOverlayStyle>(
-        value: SystemUiOverlayStyle.light,
-        child: GestureDetector(
-          child: Stack(
-            children: <Widget>[
-              Container(
-                height: double.infinity,
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Color(0x665ac18e),
-                        Color(0x995ac18e),
-                        Color(0xcc5ac18e),
-                        Color(0xff5ac18e),
-                      ]),
-                ),
-                child: SingleChildScrollView(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 25,
-                    vertical: 120,
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'Sign In',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 40,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 50,
-                      ),
-                      buildusername(),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      buildemail(),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      buildpassword(),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      buildpasswordreenter(),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      buildsignupbutton(),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
 class passreset extends StatefulWidget {
   const passreset({super.key});
 
@@ -519,7 +413,7 @@ class _navigateState extends State<navigate> {
             currentindex = index;
           });
         },
-        backgroundColor: Color(0xff5ac18e),
+        backgroundColor: const Color(0xff5ac18e),
       ),
       body: Container(
         decoration: const BoxDecoration(

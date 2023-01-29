5bece3fdac93a68a2b7925a8c3a62ffbb984f74f
hemandas630@gmail.com <hemandas630@gmail.com>
2023-01-30 02:30:21 +0545

    integrated with firebase for login

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_application_1/homepage.dart';
+import 'package:flutter_application_1/pages/Verifypage.dart';
+import 'package:flutter_application_1/pages/loginpage.dart';
+import 'package:flutter_application_1/pages/registerpage.dart';
+import 'package:flutter_application_1/pages/errormessage.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    home: Fitnex(),
+final navigatorKey = GlobalKey<NavigatorState>();
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MaterialApp(
+    scaffoldMessengerKey: messengerKey,
+    navigatorKey: navigatorKey,
+    home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Verifyemail();
+          } else {
+            return loginpage();
+          }
+        }),
   ));
 }
